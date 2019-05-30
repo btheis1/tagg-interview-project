3009e3273e4295b757ffcfcd4f38bbee0ce0e85b
C Howe <brook.theis@gmail.com>
2019-05-30 16:44:03 -0600

    built add chore form, wired front-end

--- chore-app/src/App.js
+++ chore-app/src/App.js
@@ -4,15 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import ChoreDashboard from "./components/chore-dashboard";
 import EditChore from "./components/edit-chore";
 import CreateChore from "./components/create-chore";
-
 class App extends Component {
   render() {
     return(
       <Router>
         <div className="container">
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-              <Link to="/" classname="navbar-brand"><h2>Chore Tracker App</h2></Link>
-              <div className="collapse navbar-collapse navbar-left">
+              <Link to="/" className="navbar-brand"><h2>Chore Tracker App</h2></Link>
+              <div className="navbar-right">
                 <ul className="nav navbar-nav">
                   <li className="navbar-item">
                     <Link to="/create" className="nav-link">Add Chore</Link>
--- chore-app/src/components/create-chore.js
+++ chore-app/src/components/create-chore.js
@@ -2,10 +2,76 @@ import React, { Component } from "react";
 
 
 class CreateChore extends Component {
+    state = {
+        description: "",
+        assignee: "",
+        dueDate: "",
+        completed: false
+    }
+    
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        console.log(`Form submitted:`);
+        console.log(`Description: ${this.state.description}`)
+        console.log(`Assigned to: ${this.state.assignee}`)
+        console.log(`Due date: ${this.state.dueDate}`);
+        console.log(`Completed: ${this.state.completed}`);
+
+        //backend logic here
+
+        this.setState({
+            description: "",
+            assignee: "",
+            dueDate: "",
+            completed: false
+        });
+    }
     render() {
         return (
-            <div>
+            <div className="container">
                 <h3>Add Chore Component</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.description}
+                            onChange={this.handleInputChange}
+                            name="description"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Assigned to: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.assignee}
+                            onChange={this.handleInputChange}
+                            name="assignee"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Due Date: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.dueDate}
+                            onChange={this.handleInputChange}
+                            name="dueDate"
+                        />
+                    </div>
+                    <input type="submit" 
+                        value="Add Chore"
+                        className="btn btn-primary"
+                        onClick = {this.handleFormSubmit}
+                        />
+                    
+                </form>
             </div>
         )
     }
